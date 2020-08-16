298fac8c3e45a274d4ce01396bf38c893617bdc2
lmarin263 <lmarin263@gmail.com>
2020-08-17 00:59:25 +0200

    Added static elements and basic templates

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import Cinema, Auditorium, Film, Screening
+
+
 # Create your views here.
+def index(request):
+    # Genera contadores de algunos de los objetos principales
+    num_cinemas = Cinema.objects.all().count()
+    num_auditoriums = Auditorium.objects.all().count()
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={'num_cinemas': num_cinemas, 'num_auditoriums': num_auditoriums},
+    )
