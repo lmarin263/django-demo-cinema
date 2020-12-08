5e1b4647a4a42cfb025874b2668b7015addbea89
lmarin263 <lmarin263@gmail.com>
2020-12-08 12:56:06 +0100

    Added images in Cinema object

--- cinema/models/cinema.py
+++ cinema/models/cinema.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
 
 class Cinema(models.Model):
@@ -7,6 +8,7 @@ class Cinema(models.Model):
     city_name = models.CharField(max_length=200)
     city_address = models.CharField(max_length=250)
     description = models.TextField(max_length=500, blank=True)
+    photo = models.ImageField(upload_to=settings.MEDIA_URL, blank=True)
 
     def __str__(self):
         return self.name
